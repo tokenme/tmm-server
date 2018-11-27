88bffb27dbfa3764543444b4f2a040d1d095d38c
Ted <824696468@qq.com>
2018-11-27 10:25:18 +0800

    Admin:Add share And Modifiy Article

--- handler/admin/Article.go
+++ handler/admin/Article.go
@@ -2,17 +2,15 @@ package admin
 
 import (
 	"github.com/gin-gonic/gin"
-	"strconv"
 	. "github.com/tokenme/tmm/handler"
-	"github.com/tokenme/tmm/common"
-	"github.com/ziutek/mymysql/autorc"
-	"net/http"
-	"time"
 	"fmt"
+	"net/http"
+	"github.com/tokenme/tmm/common"
 )
 
+
+/*
 type Article struct {
-	Id          int    `json:"id"  form:"id"`
 	Fileid      int    `json:"fileid" form:"fileid" `
 	Author      string `json:"author" form:"author" `
 	Title       string `json:"title" form:"title" `
@@ -25,13 +23,12 @@ type Article struct {
 	Sortid      int    `json:"sortid" form:"sortid" `
 	Published   int    `json:"published" form:"published"`
 	Online      int    `json:"online" form:"online"`
+	Id          int    `json:"id"  form:"id"`
 }
+
 
 func ModifiyArticleHandler(c *gin.Context) {
 	db := Service.Db
-	if !VerfiyAdmin(c, db) {
-		return
-	}
 	var up = Article{}
 	if CheckErr(c.Bind(&up), c) {
 		return
@@ -54,15 +51,12 @@ func ModifiyArticleHandler(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
-		"msg":  "ok",
+		"msg":  "",
 		"data": ""})
 }
 
 func AddArticleHandler(c *gin.Context) {
 	db := Service.Db
-	if !VerfiyAdmin(c, db) {
-		return
-	}
 	var article Article
 	if CheckErr(c.Bind(&article), c) {
 		return
@@ -70,31 +64,24 @@ func AddArticleHandler(c *gin.Context) {
 	Query := `INSERT INTO tmm.articles(fileid,author,title,link,source_url,
 cover,published_at,digest,content,sortid,Published) 
 VALUES (%d,'%s','%s','%s','%s','%s','%s','%s','%s',%d,%d)`
-	_, _, err := db.Query(Query, article.Fileid, db.Escape(article.Author), db.Escape(article.Title),
+	_, result, err := db.Query(Query, article.Fileid, db.Escape(article.Author), db.Escape(article.Title),
 		db.Escape(article.Link), db.Escape(article.SourceUrl), db.Escape(article.Cover),
 		db.Escape(article.PublishedOn), db.Escape(article.Digest), db.Escape(article.Content),
 		article.Sortid, 1)
 	if CheckErr(err, c) {
 		return
 	}
-	rows, result, err := db.Query(`select id from tmm.articles where link = '%s' `, db.Escape(article.Link))
-	if CheckErr(err, c) {
-		return
-	}
-	if Check(len(rows) == 0, `select Error `, c) {
-		return
-	}
-	link := fmt.Sprintf("https://tmm.tokenmama.io/article/show/%d", rows[0].Int(result.Map(`id`)))
+	link := fmt.Sprintf("https://tmm.tokenmama.io/article/show/%d", result.InsertId())
 
 	Query = `INSERT INTO tmm.share_tasks (creator, title, summary, link, 
-	image, points, points_left, bonus, max_viewers,online_status) VALUES(0, '%s', '%s', '%s', '%s', 5000, 5000, 10, 10,0)`
+	image, points, points_left, bonus, max_viewers,online_status) VALUES(0, '%s', '%s', '%s', '%s', 5000, 5000, 10, 10,-1)`
 
 	_, _, err = db.Query(Query, db.Escape(article.Title), db.Escape(article.Digest),
 		db.Escape(link), db.Escape(article.Cover))
 	if CheckErr(err, c) {
 		return
 	}
-	rows, _, err = db.Query(`select id from tmm.share_tasks where link = '%s'`, db.Escape(link))
+	rows, _, err := db.Query(`select id from tmm.share_tasks where link = '%s'`, db.Escape(link))
 	if CheckErr(err, c) {
 		return
 	}
@@ -108,39 +95,40 @@ VALUES (%d,'%s','%s','%s','%s','%s','%s','%s','%s',%d,%d)`
 
 	c.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
-		"msg":  "ok",
-		"data": ""},
+		"msg":  "",
+		"data": article},
 	)
 }
 
 func GetArticleHandler(c *gin.Context) {
 	db := Service.Db
-	if !VerfiyAdmin(c, db) {
-		return
-	}
-	var offset int
-	sortid,_ :=  strconv.Atoi(c.DefaultQuery("type","0"))
+	var (
+		offset int
+		Query  string
+		ArticleList []Article
+	)
+	sortid, _ := strconv.Atoi(c.DefaultQuery("type", "0"))
 	page, _ := strconv.Atoi(c.DefaultQuery(`page`, "1"))
 	limit, _ := strconv.Atoi(c.DefaultQuery(`limit`, "3"))
 	if page >= 1 {
-		offset = (page - 1) *limit
+		offset = (page - 1) * limit
 	} else {
 		offset = 0
 	}
-	Query := ""
+
 	if sortid == 0 {
 		Query = fmt.Sprintf(`
 	select 	id,fileid,author,
 	title,link,source_url,cover,published_at,
 	digest,content,sortid,published from tmm.articles order by published_at DESC
 	limit %d offset %d
-	`,limit,offset)
-	}else{
+	`, limit, offset)
+	} else {
 		Query = fmt.Sprintf(`
 		select 	id,fileid,author,
 		title,link,source_url,cover,published_at,
 		digest,content,sortid,published from tmm.articles 
-		where sortid = %d  limit %d offset %d`,sortid,limit,offset)
+		where sortid = %d  limit %d offset %d`, sortid, limit, offset)
 	}
 
 	Rows, Result, err := db.Query(Query)
@@ -153,12 +141,11 @@ func GetArticleHandler(c *gin.Context) {
 			"msg":  "没有到数据",
 			"data": gin.H{
 				"curr_page": page,
-				"data":      nil,
+				"data":      "",
 			},
 		})
 		return
 	}
-	ArticleList := []Article{}
 
 	for _, Row := range Rows {
 		Link := fmt.Sprintf(`https://tmm.tokenmama.io/article/show/%d`, Row.Int(Result.Map(`id`)))
@@ -186,11 +173,10 @@ func GetArticleHandler(c *gin.Context) {
 			article.Online = a[0].Int(result.Map(`online_status`))
 		}
 		ArticleList = append(ArticleList, article)
-
 	}
 	if sortid != 0 {
-		Query = fmt.Sprintf(`select count(*) from tmm.articles  where sortid = %d `,sortid)
-	}else{
+		Query = fmt.Sprintf(`select count(*) from tmm.articles  where sortid = %d `, sortid)
+	} else {
 		Query = ` select count(*) from tmm.articles`
 	}
 	Rows, _, err = db.Query(Query)
@@ -210,45 +196,15 @@ func GetArticleHandler(c *gin.Context) {
 
 }
 
-func OnlineAndOfflineHandler(c *gin.Context) {
-	db := Service.Db
-	if !VerfiyAdmin(c, db) {
-		return
-	}
-	articleId, exists := c.Get(`id`)
-	if Check(!exists, `id not can be empty`, c) {
-		return
-	}
-	status, exists := c.Get(`status`)
-	if Check(!exists, `status not can be empty`, c) {
-		return
-	}
-
-	link := fmt.Sprintf(`https://tmm.tokenmama.io/article/show/%d`, articleId)
-	Query := `update tmm.share_tasks set online_status = %d where link = '%s'`
-	_, _, err := db.Query(Query, status, link)
-	if CheckErr(err, c) {
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"code": 200,
-		"Msg":  "ok",
-		"data": nil,
-	})
-}
-
 func DeleteArticleHandler(c *gin.Context) {
 	db := Service.Db
-	if !VerfiyAdmin(c, db) {
-		return
-	}
 	articleId, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if CheckErr(err, c) {
 		return
 	}
 	link := fmt.Sprintf(`https://tmm.tokenmama.io/article/show/%d`, articleId)
 	_, _, err = db.Query(`delete tmm.share_tasks  ,tmm.share_task_categories  from tmm.share_tasks,tmm.share_task_categories
-	 where tmm.share_tasks.link = '%s' 
+	 where tmm.share_tasks.link = '%s'
 	 and  tmm.share_task_categories.task_id = tmm.share_tasks.id
 		`, db.Escape(link))
 	if CheckErr(err, c) {
@@ -261,29 +217,96 @@ func DeleteArticleHandler(c *gin.Context) {
 	_, _, err = db.Query(`delete from `)
 	c.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
-		"msg":  "ok",
+		"msg":  "",
 		"data": "",
 	})
 
 }
+*/
 
-func VerfiyAdmin(c *gin.Context, db *autorc.Conn) bool {
-	userContext, exists := c.Get("USER")
-	if Check(!exists, `Need login`, c) {
-		return false
+type Online struct {
+	Id        int  `json:"id" form:"id"`
+	IsArticle bool `json:"is_article" from:"is_article"`
+	Status    int  `json:"status" form:"status"`
+}
+
+func OnlineAndOfflineHandler(c *gin.Context) {
+	db := Service.Db
+	var On Online
+	if CheckErr(c.Bind(&On), c) {
+		return
 	}
-	User := userContext.(common.User)
-	Query := `select role from user_settings where user_id = %d`
-	Rows, _, err := db.Query(Query, User.Id)
+	if On.IsArticle {
+		link := fmt.Sprintf(`https://tmm.tokenmama.io/article/show/%d`, On.Id)
+		Query := `update tmm.share_tasks set online_status = %d where link = '%s'`
+		_, _, err := db.Query(Query, On.Status, link)
+		if CheckErr(err, c) {
+			return
+		}
+	} else {
+		_, _, err := db.Query(`update tmm.share_tasks set online_status = %d where id = %d`, On.Status, On.Id)
+		if CheckErr(err, c) {
+			return
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "",
+		"data": "",
+	})
+}
+
+
+func GetTypeHander(c *gin.Context) {
+	db := Service.Db
+
+	Query := `select id,name from tmm.article_categories `
+	maps := make(map[int]string)
+	Rows, result, err := db.Query(Query)
 	if CheckErr(err, c) {
-		return false
+		return
 	}
-	if Check(len(Rows) == 0, `没有查询到数据`, c) {
-		return false
+	for _, Row := range Rows {
+		maps[Row.Int(result.Map(`id`))] = Row.Str(result.Map(`name`))
 	}
-	row := Rows[0]
-	if row.Int(0) != 1 {
-		return false
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"msg":  "",
+		"data": maps},
+	)
+
+}
+
+func VerfiyAdminFunc() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := Service.Db
+		userContext, exists := c.Get("USER")
+		if Check(!exists, `Need login`, c) {
+			return
+		}
+		User := userContext.(common.User)
+		Query := `select role from user_settings where user_id = %d`
+		Rows, _, err := db.Query(Query, User.Id)
+		if len(Rows) == 0 || err != nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": `401`,
+				"msg":  "invalid User",
+			})
+			return
+		}
+		row := Rows[0]
+		if row.Int(0) != 1 {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				`code`: `401`,
+				`msg`:  `The User Must Be Admin`,
+			})
+			return
+		}
+		c.Next()
+		return
 	}
-	return true
+
 }
--- router/admin.go
+++ router/admin.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/tokenme/tmm/handler/admin"
 	"net/http"
+	"github.com/tokenme/tmm/handler/qiniu"
 )
 
 func AdminRouter(r *gin.Engine) {
@@ -12,20 +13,31 @@ func AdminRouter(r *gin.Engine) {
 	AdminAuth.Unauthorized = func(c *gin.Context, code int, message string) {
 		c.JSON(http.StatusOK, gin.H{
 			"code":    code,
-			"message": message,
-		})
+			"message": message,})
 	}
 	r.POST(`/admin/login`, AdminAuth.LoginHandler)
 	AdminGroup.Use(AuthMiddleware.MiddlewareFunc())
+
+	/* @Router: admin/article 						*/
 	Article := AdminGroup.Group(`/article`)
-	Article.Use(AuthMiddleware.MiddlewareFunc())
+	Article.Use(AuthMiddleware.MiddlewareFunc(), admin.VerfiyAdminFunc())
 	{
-		Article.POST(`/add`, admin.AddArticleHandler)
+		//Article.POST(`/add`, admin.AddArticleHandler)
+		//Article.POST(`/modifiy`, admin.ModifiyArticleHandler)
+		//Article.DELETE(`/delete/:id`, admin.DeleteArticleHandler)
+		//Article.GET(`/list`, admin.GetArticleHandler)
 		Article.GET(`/info`, admin.GetInfoHandler)
-		Article.GET(`/list`, admin.GetArticleHandler)
-		Article.POST(`/modifiy`, admin.ModifiyArticleHandler)
-		Article.DELETE(`/delete/:id`, admin.DeleteArticleHandler)
 		Article.POST(`/online`, admin.OnlineAndOfflineHandler)
+		Article.GET(`/type`, admin.GetTypeHander)
+	}
+
+	/* @Router: admin/share 						*/
+	share := AdminGroup.Group(`/share`)
+	share.Use(AuthMiddleware.MiddlewareFunc(), admin.VerfiyAdminFunc())
+	{
+		share.GET(`/getToken`, qiniu.UpTokenHandler)
+		share.POST(`/add`, admin.AddShareHandler)
+		share.GET(`/list`, admin.GetShareListHandler)
 	}
 
 }
