b042129e684802301d1d7e604ff846392e876bdb
Syd Xu <prof.syd.xu@gmail.com>
2018-12-28 09:47:55 +0000

    debug device/ping

--- handler/device/ping.go
+++ handler/device/ping.go
@@ -38,9 +38,7 @@ func PingHandler(c *gin.Context) {
 		return
 	}
 	if Check(pingRequest.Device.IsEmulator || pingRequest.Device.IsJailBrojen, "invalid request", c) {
-		return
-	}
-	if Check(pingRequest.Ts > 70, "invalid request", c) {
+		log.Error("ping is emulator or jailbrojen")
 		return
 	}
 
@@ -53,11 +51,30 @@ func ping(service *common.Service, pingRequest common.PingRequest) {
 	deviceId := device.DeviceId()
 	appId := device.AppId()
 
+	db := service.Db
 	if !validatePingRequest(pingRequest, deviceId, appId, service) {
+		query := `UPDATE
+                tmm.devices AS d,
+                tmm.device_apps AS da,
+                tmm.apps AS a
+            SET
+                d.lastping_at=NOW(),
+                da.lastping_at=NOW(),
+                a.lastping_at=NOW(),
+                a.is_active=1
+            WHERE
+                da.device_id=d.id
+            AND d.id='%s'
+            AND da.app_id=a.id AND a.id='%s'
+            AND a.platform='%s'`
+		_, _, err := db.Query(query, db.Escape(deviceId), db.Escape(appId), device.Platform)
+		if err != nil {
+			raven.CaptureError(err, nil)
+			log.Error(err.Error())
+		}
 		return
 	}
 
-	db := service.Db
 	query := `UPDATE
                 tmm.devices AS d,
                 tmm.device_apps AS da,
@@ -93,6 +110,7 @@ func ping(service *common.Service, pingRequest common.PingRequest) {
 	row := rows[0]
 	tmpTs := row.ForceFloat(0)
 	if tmpTs < float64(Config.MinGrowthTS) {
+		log.Warn("tmpTs less than min growth ts, %v, Device:%s, AppId:%s", tmpTs, deviceId, appId)
 		return
 	}
 	tmmBalance, _ := decimal.NewFromString(row.Str(1))
@@ -122,7 +140,25 @@ func ping(service *common.Service, pingRequest common.PingRequest) {
 }
 
 func validatePingRequest(pingRequest common.PingRequest, deviceId string, appId string, service *common.Service) bool {
-	if pingRequest.Ts <= 0 || pingRequest.Logs == "" || deviceId == "" || appId == "" {
+	//if pingRequest.Ts <= 0 || pingRequest.Logs == "" || deviceId == "" || appId == "" {
+	if pingRequest.Ts <= 0 || deviceId == "" || appId == "" {
+		if pingRequest.Ts <= 0 {
+			log.Error("ping ts is empty")
+		}
+		if pingRequest.Logs == "" {
+			log.Error("ping empty logs")
+		}
+		if deviceId == "" {
+			log.Error("ping empty deviceId")
+		}
+		if appId == "" {
+			log.Error("ping empty appId")
+		}
+		return false
+	}
+
+	if pingRequest.Ts > 80 {
+		log.Error("ping too fast: %d, Device:%s, AppId:%s", pingRequest.Ts, deviceId, appId)
 		return false
 	}
 	redisConn := service.Redis.Master.Get()
@@ -132,18 +168,18 @@ func validatePingRequest(pingRequest common.PingRequest, deviceId string, appId 
 	nowTs := time.Now().Unix()
 	var cacheData common.PingCache
 	json.Unmarshal(cachejs, &cacheData)
-	if nowTs-cacheData.Ts < 60 {
+	if nowTs-cacheData.Ts < 50 {
 		log.Warn("Too fast Ping %d, Device:%s, AppId:%s", nowTs-cacheData.Ts, deviceId, appId)
 		return false
 	}
-	if cacheData.Cap > 120 {
+	if cacheData.Cap > 180 {
 		log.Warn("PingCap: %d, Device:%s, AppId:%s", cacheData.Cap, deviceId, appId)
 		return false
 	}
 	logReq := utils.Sha1(pingRequest.Logs)
 	if logReq == cacheData.Logs {
-		log.Warn("PingLog: %s, Device:%s, AppId:%s", logReq, deviceId, appId)
-		return false
+		//log.Warn("PingLog: %s, Device:%s, AppId:%s", logReq, deviceId, appId)
+		//return false
 	}
 	js, err := json.Marshal(common.PingCache{
 		Ts:   nowTs,
